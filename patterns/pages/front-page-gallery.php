8d68461e1f04a884c3194fb0bd7c52a81655f60a
beckej13820 <beckej@alumni.lemoyne.edu>
2025-08-02 17:37:02 -0400

    Add 2 More Frontpage patterns

---
+++ patterns/pages/front-page-gallery.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Title: Front page gallery
+ * Slug: red-dragon-portfolio/front-page-gallery
+ * Categories: red_dragon_portfolio_page
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: An about me pattern.
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Large Banner Image and text","categories":["red_dragon_portfolio_portfolio-banners"],"patternName":"red-dragon-portfolio/large-banner-image-and-text"},"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull"><!-- wp:cover {"url":"http://localhost:8881/wp-content/themes/red-dragon-portfolio/assets/images/milne-plaza-feature-image-1-1024x683.jpg","dimRatio":60,"overlayColor":"contrast","isUserOverlayColor":true,"contentPosition":"center center","isDark":false,"sizeSlug":"large","align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull is-light has-base-color has-text-color has-link-color" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><img class="wp-block-cover__image-background size-large" alt="" src="http://localhost:8881/wp-content/themes/red-dragon-portfolio/assets/images/milne-plaza-feature-image-1-1024x683.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-60 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","className":"is-style-default","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-text-align-center is-style-default has-xx-large-font-size">Red Dragon</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Future Dietitian passionate about sustainability</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"align":"center"} -->
+<ul class="wp-block-social-links aligncenter"><!-- wp:social-link {"url":"linkedin.com","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"email@example.com","service":"mail"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"Instagram square gallery","categories":["gallery"],"patternName":"red-dragon-portfolio/gallery-with-instagram-inspired-squares"},"align":"full","className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"grid","minimumColumnWidth":"18rem"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-x-large-font-size">Describe your gallery</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8881/wp-content/themes/red-dragon-portfolio/assets/images/oneonta_01.jpg" alt="" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8881/wp-content/themes/red-dragon-portfolio/assets/images/08-7455-1.jpg" alt="" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8881/wp-content/themes/red-dragon-portfolio/assets/images/university-designation-dec-2022-news-slider.jpg.webp" alt="" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8881/wp-content/themes/red-dragon-portfolio/assets/images/oneonta_01.jpg" alt="" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8881/wp-content/themes/red-dragon-portfolio/assets/images/oneonta_01.jpg" alt="" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8881/wp-content/themes/red-dragon-portfolio/assets/images/08-7455-1.jpg" alt="" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"lightbox":{"enabled":true},"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost:8881/wp-content/themes/red-dragon-portfolio/assets/images/oneonta_01.jpg" alt="" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"categories":["text"],"patternName":"red-dragon-portfolio/heading-and-left-aligned-text","name":"Heading and left aligned text"},"align":"full","className":"is-style-section-1","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-section-1" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:heading -->
+<h2 class="wp-block-heading">Heading</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"large"} -->
+<p class="has-large-font-size">Quisque tellus dui, consectetur ut nulla sit amet, dapibus ullamcorper nisl. Nunc imperdiet elit eu dolor ornare, et tempus mi congue. Proin vitae libero id lorem rutrum molestie quis in tellus. Pellentesque vel turpis luctus, auctor enim vel, eleifend sapien. Aliquam vitae posuere felis, non elementum enim. Pellentesque scelerisque, augue et fringilla mattis, justo eros rhoncus lacus, et tincidunt lacus nulla vel massa. Suspendisse potenti. Fusce eu lacinia velit, id ullamcorper lacus. Vestibulum a nulla venenatis, elementum mauris eu, tempus nibh. Donec ut metus nec orci porttitor euismod.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
---
+++ patterns/pages/scenic.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Title: Scenic
+ * Slug: red-dragon-portfolio/scenic
+ * Categories: red_dragon_portfolio_page
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: An about me pattern.
+ */
+?>
+
+<!-- wp:pattern {"slug":"red-dragon-portfolio/background-image-and-textbox"} /-->
+ <!-- wp:group {"metadata":{"categories":["text"],"patternName":"red-dragon-portfolio/heading-and-left-aligned-text","name":"Heading and left aligned text"},"align":"full","className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:heading -->
+<h2 class="wp-block-heading">Heading</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"large"} -->
+<p class="has-large-font-size">Quisque tellus dui, consectetur ut nulla sit amet, dapibus ullamcorper nisl. Nunc imperdiet elit eu dolor ornare, et tempus mi congue. Proin vitae libero id lorem rutrum molestie quis in tellus. Pellentesque vel turpis luctus, auctor enim vel, eleifend sapien. Aliquam vitae posuere felis, non elementum enim. Pellentesque scelerisque, augue et fringilla mattis, justo eros rhoncus lacus, et tincidunt lacus nulla vel massa. Suspendisse potenti. Fusce eu lacinia velit, id ullamcorper lacus. Vestibulum a nulla venenatis, elementum mauris eu, tempus nibh. Donec ut metus nec orci porttitor euismod.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"List with Heading, 4 items and descriptions","categories":["text"],"patternName":"red-dragon-portfolio/list-with-heading-4-items-and-descriptions"},"align":"full","className":"is-style-section-1","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-section-1" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading -->
+<h2 class="wp-block-heading">You can edit this text to describe yourself and your work.</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"border":{"radius":"10px"}},"layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"14rem"}} -->
+<div class="wp-block-group" style="border-radius:10px"><!-- wp:group {"style":{"border":{"radius":"10px","width":"3px"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"layout":{"columnSpan":1,"rowSpan":1}},"borderColor":"accent-5","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-border-color has-accent-5-border-color" style="border-width:3px;border-radius:10px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size">What I do</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.&nbsp;</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"radius":"10px","width":"3px"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"borderColor":"accent-5","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-border-color has-accent-5-border-color" style="border-width:3px;border-radius:10px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size">Vehicula orci nec</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Quisque sit amet tortor enim.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"radius":"10px","width":"3px"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"borderColor":"accent-5","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-border-color has-accent-5-border-color" style="border-width:3px;border-radius:10px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size">Ornare nibh et</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Sed pellentesque nisl tortor, blandit sagittis dolor aliquet nec.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"radius":"10px","width":"3px"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"borderColor":"accent-5","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-border-color has-accent-5-border-color" style="border-width:3px;border-radius:10px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size">Vestibulum vehicula ultrices augue</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Vivamus dapibus sollicitudin nisl, ut scelerisque orci porta sit amet.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
