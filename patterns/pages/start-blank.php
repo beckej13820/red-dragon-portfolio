fa89b9d30b87a694a12bf38a6fcab27cde9d4d99
Ed Beck <beckej@alumni.lemoyne.edu>
2025-09-25 09:12:19 -0400

    add an empty pattern

---
+++ patterns/pages/start-blank.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Start blank
+ * Slug: red-dragon-portfolio/start-blank
+ * Categories: red_dragon_portfolio_page
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: A blank pattern to get started.
+ */
+?>
+
