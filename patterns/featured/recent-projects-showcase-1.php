57ec3c3bb9b931548bd68ef70a6e3a3668aee19b
Ed Beck <beckej@alumni.lemoyne.edu>
2026-02-22 12:08:42 -0500

    revert

--- patterns/featured/recent-projects-showcase-1.php
+++ patterns/featured/recent-projects-showcase-1.php
@@ -27,7 +27,7 @@
 <!-- wp:group {"align":"wide","fontSize":"medium","layout":{"type":"default"}} -->
 <div class="wp-block-group alignwide has-medium-font-size"><!-- wp:paragraph {"align":"right"} -->
 <p class="has-text-align-right"><?php /* Translators: 1. is the start of a 'a' HTML element, 2. is the end of a 'a' HTML element */ 
-echo sprintf( esc_html__( '%1$sMore Projects%2$s', 'red-dragon-portfolio' ), '<a href="' . esc_url( '/projects/' ) . '</a>' ); ?></p>
+echo sprintf( esc_html__( '%1$sMore Projects%2$s', 'red-dragon-portfolio' ), '<a href="' . esc_url( '/posts/' ) . '" data-type="page" data-id="254">', '</a>' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
